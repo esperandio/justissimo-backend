8db9c1dfc410ad076a76942e8e065185a2e1c500
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-05-04 22:19:51 -0300

    chore: criacao de rota de teste para validar action do github

--- src/routes/heloworld.ts
+++ src/routes/heloworld.ts
@@ -4,6 +4,10 @@ import prisma from '../database/index'
 const route = Router()
 
 route.get('/', async(req: Request, res: Response) => {
+    res.status(200).json("OK")
+})
+
+route.get('/users', async(req: Request, res: Response) => {
     const dados = await prisma.user.findMany();
     res.status(200).json({ message: dados})
 })
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(route)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => 'server running on port 3333')
