f0b2dd7746a9b7fe20a22a70ea0601c64a831223
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-05-08 13:36:15 -0300

    feat: ajustado o error handler do express para que o status http em casos de erro seja com base no tipo de erro lancado

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,31 @@
 
 import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
-import {router} from './routes'
+import { router } from './routes'
+import { NotFoundError, DomainError } from "./errors";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    return response.status(400).json({
-        status: "Error",
-        message: error.message,
+    if (error instanceof NotFoundError) {
+        return response.status(404).json({
+            status: "Not found",
+            message: error.message,
+        });
+    }
+
+    if (error instanceof DomainError) {
+        return response.status(400).json({
+            status: "Bad request",
+            message: error.message,
+        });
+    }
+
+    return response.status(500).json({
+        status: "Internal Server Error",
+        message: error.message
     });
 });
 
