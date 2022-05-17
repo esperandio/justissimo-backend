7521bca66017beef0b7f3227a9ca5a6de641e674
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-05-16 22:29:49 -0300

    feat: criacao de erro para advogado nao encontrado

--- src/errors/client-not-found-error.ts
+++ src/errors/client-not-found-error.ts
@@ -2,6 +2,6 @@ import { NotFoundError } from "./not-found-error"
 
 export class ClientNotFoundError extends NotFoundError {
     constructor () {
-        super("Client not found.")
+        super("Cliente não encontrado.")
     }
 }
--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,5 @@
 export * from './not-found-error';
 export * from './client-not-found-error';
+export * from './lawyer-not-found-error';
 export * from './user-not-found-error';
 export * from './domain-error';
