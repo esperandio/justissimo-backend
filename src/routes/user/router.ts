d13c8dddb647f89b725ba00a09e5fed988bfeea4
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-05-21 14:16:10 -0300

    refactor: adicionada novamente as rotas de recuperacao de senha (apos realinhamento da branch)

--- src/routes/user/router.ts
+++ src/routes/user/router.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { ensureAuthenticated } from "../../middlewares/ensureAutheticated";
 import { AuthenticateUserController } from "../../useCases/authenticateUser/AuthenticateUserController";
 import { DeleteUserController } from "../../useCases/deleteUser/DeleteUserController";
+import { PasswordRecoveryController } from "../../useCases/passwordRecovery/PasswordRecoveryController";
+import { ValidatePasswordRecoveryCodeController } from "../../useCases/validatePasswordRecoveryCode/ValidatePasswordRecoveryCodeController";
 
 const router = Router();
 /**User*/
 router.delete("/user/:id", ensureAuthenticated, new DeleteUserController().handle);
+router.post('/users/password/recovery', new PasswordRecoveryController().handle);
+router.get("/users/password/recovery/:recoveryCode", new ValidatePasswordRecoveryCodeController().handle);
 router.post('/login', new AuthenticateUserController().handle);
 
 export { router as router_user };
