559ced766c9ff346be459b30041d0ae339a3eb6c
David-Jesus <davidjesus357@gmail.com>
2022-06-11 09:50:23 -0300

    feat: definicao dos dados necessarios para o agendamento.

--- src/useCases/createScheduling/CreateSchedulingUseCase.ts
+++ src/useCases/createScheduling/CreateSchedulingUseCase.ts
@@ -1,7 +1,19 @@
 
+interface IUserRequest {
+    fk_advogado: number;
+    fk_cliente: number;
+    fk_advogado_area: number;
+    causa: string;    
+    data_agendamento: string;
+    duracao: number;     
+    horario: string;  
+    dia: string;        
+    observacao: string;    
+    contato_cliente: string;
 
+}
 class CreateSchedulingUseCase {
-    async execute() {
+    async execute(userRequest: IUserRequest) {
 
     }
 }
