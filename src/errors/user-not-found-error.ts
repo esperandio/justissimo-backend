0943d65b152c37ad8de54c885d382d5747a7e011
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-19 20:20:26 -0300

    fix: correcao em mensagem de erro lancada pela aplicacao quando usuario nao encontrado

--- src/errors/user-not-found-error.ts
+++ src/errors/user-not-found-error.ts
@@ -2,6 +2,6 @@ import { NotFoundError } from "./not-found-error"
 
 export class UserNotFoundError extends NotFoundError {
     constructor () {
-        super("User not found.")
+        super("Usuário não encontrado.")
     }
 }
