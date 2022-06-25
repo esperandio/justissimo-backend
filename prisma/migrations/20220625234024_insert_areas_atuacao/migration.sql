21225652dc0c86b91e17dc20e9bb9f051d57b6c2
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-25 20:50:01 -0300

    feat: criacao de migration para inserir areas de atuacao

---
+++ prisma/migrations/20220625234024_insert_areas_atuacao/migration.sql
@@ -0,0 +1,1 @@
+insert into area_atuacao (titulo) values ('Direito Civil'), ('Direito Ambiental'), ('Direito Empresarial'), ('Direito da Tecnologia da Informação'), ('Direito do Consumidor'), ('Direito Contratual'), ('Direito Penal'), ('Direito Trabalhista'), ('Direito Tributário'), ('Direito Administrativo'), ('Direito Comercial');
