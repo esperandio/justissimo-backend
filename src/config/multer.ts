6389e57a4e1cc65ce23e8a680f2c4643b296afe8
Carlos Daniel <davidjesus357@gmail.com>
2022-09-07 21:25:09 -0300

    feat: definicao das configuracoes do multer.

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,36 +1,61 @@
 import multer from "multer";
 import path from "path";
 import crypto from "crypto";
+import * as AWS from "aws-sdk";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import encrypt from "./utils/crypto";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const storegeTypes = {
-    local: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
-        },
-        filename: (req, file, cb) => {
-            crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err, file.originalname);
-                const fileName = `${hash.toString("hex")}-${file.originalname}`;
-                cb(null, fileName);
-                });
-            }
-        })
+let s3 = new S3Client({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
     }
+});
 
-    // multerS3({
-    //     S3: new aws.S3(),
-    //     bucket: "justissimo",
-    //     contentType: multerS3.AUTO_CONTENT_TYPE,
-    //     acl: "public-read",
-    //     key: (req, file, cb) => {
-    //         crypto.randomBytes(16, (err, hash) => {
-    //             if (err) cb(err, file.originalname);
-    //             const fileName = `${hash.toString("hex")}-${file.originalname}`;
-    //             cb(null, fileName);
-    //         });
-    //     }
-    // })
+const storageTypes = {
+    local: multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, "./uploads/");
+        },
+        filename: function (req, file, cb) {
+          cb(null, encrypt(new Date().toLocaleString()) + "-" + file.originalname);
+        },
+      }),
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+    s3: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET || "",
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: "public-read",
+        key: (req, file, cb) => {
+            const fileName = encrypt(new Date().toLocaleString()) + "-" + file.originalname;
+            cb(null, fileName);
+        },
+    })
+}
+
+const fileFilter = (
+  req: any,
+  file: { mimetype: string },
+  cb: (arg0: null, arg1: boolean) => void
+) => {
+
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const uploadImage = multer({
+  storage: storageTypes['s3'],
+  limits: {
+    fileSize: 1024 * 1024 * 3,
+    fieldNameSize: 100,
+  },
+  fileFilter: fileFilter,
+});
+
+export default uploadImage;
