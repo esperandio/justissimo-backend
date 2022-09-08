a08a16a485ce6cd95df3f917f74d429be08de929
Carlos Daniel <davidjesus357@gmail.com>
2022-09-07 22:31:12 -0300

    ref: removido console.log

--- src/useCases/listUserProfile/ListUserProfileController.ts
+++ src/useCases/listUserProfile/ListUserProfileController.ts
@@ -7,8 +7,6 @@ import { ListUserProfileUseCase } from "./ListUserProfileUseCase";
         const { id } = request.params;
         const type_user = request.query.tipo_usuario as string;
 
-        console.log(id);
-        console.log(type_user);
         const  userProfile = await new ListUserProfileUseCase().execute( Number.parseInt(id), type_user);
     
         return  response.json(userProfile);
