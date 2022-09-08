8761ee7184568703bad44721e7e96b06995cece9
Carlos Daniel <davidjesus357@gmail.com>
2022-09-07 21:27:04 -0300

    feat: adicionado upload de imagem de clientes

--- src/routes/clients/router.ts
+++ src/routes/clients/router.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import multer from "multer";
+import uploadImage from "../../config/multer";
 import { ensureAuthenticated } from "../../middlewares/ensureAutheticated";
 import { CreateUserClientController } from "../../useCases/createUserClient/CreateUserClientController";
 import { ListAllClientsController } from "../../useCases/listUserClient/listAllClients/ListAllClientsController";
 import { ListClientByIdController } from "../../useCases/listUserClient/listClientById/ListClientByIdController";
 import { CreateDivulgationController } from "../../useCases/createDivulgation/CreateDivulgationController";
 import { CreateSchedulingController } from "../../useCases/createScheduling/CreateSchedulingController";
+import encrypt from "../../config/utils/crypto";
 
 const router = Router();
 
 /**Clients*/
-router.post("/clients", multer().single('file'),new CreateUserClientController().handle);
+router.post("/clients", uploadImage.single('file'), new CreateUserClientController().handle) ;
 router.get("/clients", new ListAllClientsController().handle);
 router.get("/clients/:id",ensureAuthenticated, new ListClientByIdController().handle);
 
