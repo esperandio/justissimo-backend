edc09e837c4484e6036c7fa39fad3bfadabadd16
Carlos Daniel <davidjesus357@gmail.com>
2022-09-24 11:42:17 -0300

    feat: finalizado implemtacao da listagem de todas as divulgacoes juntamente com filtros necessarios.

--- src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationUseCase.ts
+++ src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationUseCase.ts
@@ -1,12 +1,54 @@
-import { Divulgacao } from "@prisma/client";
 import { prisma } from "../../../database";
+import { DomainError } from "../../../errors";
+import { NonEmptyString } from "../../../validators";
 
+interface IListDivulgationRequest{
+    date_init: string;
+    date_final: string;
+    area: string;
+}
 
 class ListAllDivulgationUseCase {
+  async execute(listDivulgationRequest: IListDivulgationRequest) {
+    let filterDateInit  = new Date(-8640000000000);
+    let filterDateFinal = new Date(8640000000000);
+    let filterArea = {};
 
-  async execute() {
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.date_init)) {
+        filterDateInit = new Date(listDivulgationRequest.date_init  + "T00:00:00.000Z");
+    }
+    
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.date_final)) {
+        filterDateFinal = new Date(listDivulgationRequest.date_final + "T23:59:59.000Z");
+    }
+    
+    if (isNaN(filterDateInit.getTime())) {
+        throw new DomainError('Campo data_inicial inválido, esperado uma data válida, recebido: ' + listDivulgationRequest.date_init);
+    }
+
+    if (isNaN(filterDateFinal.getTime())) {
+        throw new DomainError('Campo data_final inválido, esperado uma data válida, recebido: ' + listDivulgationRequest.date_final);
+    }
+
+    if (filterDateInit.getTime() > filterDateFinal.getTime()) {
+        throw new DomainError('Filtro inválido pois a data inicial é maior que a data final!');
+    }    
+
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.area)) {
+        filterArea = { equals: Number.parseInt(listDivulgationRequest.area) }
+    }
+    console.log(filterDateFinal, filterDateInit);
 
     const divulgations = await prisma.divulgacao.findMany({
+        
+        where:{
+            encerrado: false,
+            dt_cadastro: {
+                gte: filterDateInit,
+                lte: filterDateFinal
+            },
+            fk_area_atuacao: filterArea
+        },
         include: {
             cliente: true,
             areaAtuacao: {
@@ -16,7 +58,7 @@ class ListAllDivulgationUseCase {
             }
         }
     });
-    console.log(divulgations);
+
     return divulgations;
   }
 }
