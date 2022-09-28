0a4c95118da130133d27f8fa08628e52d93a0180
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-09-27 21:40:03 -0300

    feat: adicionado filtros para a busca de divulgacoes.

--- src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationController.ts
+++ src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationController.ts
@@ -3,9 +3,17 @@ import { ListAllDivulgationUseCase } from "./ListAllDivulgationUseCase";
 
 
 class ListAllDivulgationController {
-
   async handle(request: Request, response: Response): Promise<Response> {
-      const divulgations =  await new ListAllDivulgationUseCase().execute();
+    const data_inicial   = request.query.data_inicial as string;
+    const data_final = request.query.data_final as string;
+    const area = request.query.area as string;
+
+      const divulgations =  await new ListAllDivulgationUseCase().execute({
+        date_init: data_inicial,
+        date_final: data_final,
+        area
+      });
+      
       return response.status(200).json(divulgations);
 
   }
--- src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationUseCase.ts
+++ src/useCases/listDivulgation/listAllDivulgation/ListAllDivulgationUseCase.ts
@@ -1,14 +1,64 @@
-import { Divulgacao } from "@prisma/client";
 import { prisma } from "../../../database";
+import { DomainError } from "../../../errors";
+import { NonEmptyString } from "../../../validators";
 
+interface IListDivulgationRequest{
+    date_init: string;
+    date_final: string;
+    area: string;
+}
 
 class ListAllDivulgationUseCase {
+  async execute(listDivulgationRequest: IListDivulgationRequest) {
+    let filterDateInit  = new Date(-8640000000000);
+    let filterDateFinal = new Date(8640000000000);
+    let filterArea = {};
 
-  async execute() {
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.date_init)) {
+        filterDateInit = new Date(listDivulgationRequest.date_init  + "T00:00:00.000Z");
+    }
+    
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.date_final)) {
+        filterDateFinal = new Date(listDivulgationRequest.date_final + "T23:59:59.000Z");
+    }
+    
+    if (isNaN(filterDateInit.getTime())) {
+        throw new DomainError('Campo data_inicial inválido, esperado uma data válida, recebido: ' + listDivulgationRequest.date_init);
+    }
+
+    if (isNaN(filterDateFinal.getTime())) {
+        throw new DomainError('Campo data_final inválido, esperado uma data válida, recebido: ' + listDivulgationRequest.date_final);
+    }
+
+    if (filterDateInit.getTime() > filterDateFinal.getTime()) {
+        throw new DomainError('Filtro inválido pois a data inicial é maior que a data final!');
+    }    
+
+    if (!NonEmptyString.isEmpty(listDivulgationRequest.area)) {
+        filterArea = { equals: Number.parseInt(listDivulgationRequest.area) }
+    }
 
     const divulgations = await prisma.divulgacao.findMany({
+        
+        where:{
+            encerrado: false,
+            dt_cadastro: {
+                gte: filterDateInit,
+                lte: filterDateFinal
+            },
+            fk_area_atuacao: filterArea
+        },
         include: {
-            cliente: true,
+            cliente: {
+                select: {
+                    nome: true,
+                    usuario: {
+                        select: {
+                            url_foto_perfil: true,
+                        }
+                    }
+                }
+            },
             areaAtuacao: {
                 select: {
                     titulo: true,
@@ -16,7 +66,7 @@ class ListAllDivulgationUseCase {
             }
         }
     });
-    console.log(divulgations);
+
     return divulgations;
   }
 }
