4960064edab446d225f41b19f302569db3cbc070
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-10-01 11:22:02 -0300

    ref: ajustado endpoint para padronizacao

--- src/routes/clients/router.ts
+++ src/routes/clients/router.ts
@@ -7,6 +7,7 @@ import { ListClientByIdController } from "../../useCases/listUserClient/listClie
 import { CreateDivulgationController } from "../../useCases/createDivulgation/CreateDivulgationController";
 import { CreateSchedulingController } from "../../useCases/createScheduling/CreateSchedulingController";
 import { ListAllDivulgationController } from "../../useCases/listDivulgation/listDivulgationClient/ListAlldivulgationControler";
+import { ListMessagesDivulgationController } from "../../useCases/listMessagesDivulgation/ListMessagesDivulgationClientController";
 
 const router = Router();
 
@@ -14,6 +15,7 @@ const router = Router();
 router.get("/clients", new ListAllClientsController().handle);
 router.get("/clients/:id",ensureAuthenticated, new ListClientByIdController().handle);
 router.get("/clients/divulgations/:id",ensureAuthenticated, new ListAllDivulgationController().handle);
+router.get("/clients/divulgation/:id",ensureAuthenticated, new ListMessagesDivulgationController().handle);
 
 router.post("/clients", uploadImage.single('file'), new CreateUserClientController().handle) ;
 router.post("/clients/:id/divulgations", new CreateDivulgationController().handle);
