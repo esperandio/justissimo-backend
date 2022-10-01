67eebdbeeaff6a06a029403e1c4c50978a8d0b6d
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-10-01 11:23:02 -0300

    ref: ajustado consulta para cliente especifico e renomeacao dos arquivos

---
+++ src/useCases/listMessagesDivulgation/ListMessagesDivulgationClientController.ts
@@ -0,0 +1,19 @@
+import { Request, Response} from "express";
+import { ListMessageClientUseCase } from "./ListMessagesDivulgationClientUseCase";
+
+class ListMessagesDivulgationController {
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { fk_cliente } = request.body;
+    
+    const messages = await new ListMessageClientUseCase().execute({
+        id_divulgation: id,
+        fk_client: fk_cliente
+    });
+
+    return response.json(messages);
+  }
+}
+
+export { ListMessagesDivulgationController }
---
+++ src/useCases/listMessagesDivulgation/ListMessagesDivulgationClientUseCase.ts
@@ -0,0 +1,67 @@
+import { MensagemDivulgacao } from "@prisma/client";
+import { prisma } from "../../database";
+import { DomainError } from "../../errors";
+import { DivulgationNotFoundError } from "../../errors/divulgation-not-found-error";
+import { NonEmptyString } from "../../validators";
+
+interface IListMessagesDivulgationRequest {
+    id_divulgation: string;
+    fk_client: string;
+}
+
+class ListMessageClientUseCase{
+    async execute(listMessagesRequest: IListMessagesDivulgationRequest) {
+
+        const id_divulgation = NonEmptyString.validate("id_divulgation", listMessagesRequest.id_divulgation).value;
+
+        const fk_client = NonEmptyString.validate("fk_client", listMessagesRequest.fk_client).value;
+
+        if (Number.parseInt(id_divulgation) <= 0) {
+            throw new DomainError("Id da divulgacao invalido!");
+        }
+
+        if (Number.parseInt(fk_client) <= 0) {
+            throw new DomainError("Id do cliente invalido!");
+        }
+        
+        const messages = await prisma.divulgacao.findMany({
+            where: {
+                id_divulgacao: Number.parseInt(id_divulgation)
+            },
+            select: {
+                id_divulgacao: true,
+                titulo: true,
+                descricao: true,
+                dt_cadastro: true,
+                encerrado: true,
+                mensagens: {
+                    select: {
+                        id_mensagem_divulgacao: true,
+                        mensagem: true,
+                        dt_mensagem: true,
+                        advogado: {
+                            select: {
+                                id_advogado: true,
+                                nome: true,
+                            }
+                        },
+                    },
+                    where: {
+                        fk_cliente: Number.parseInt(fk_client)
+                    },
+                    orderBy: {
+                        dt_mensagem: "desc"
+                    }
+                }
+            }
+        });
+        
+        if (messages.length === 0) {
+            throw new DivulgationNotFoundError();
+        }
+
+        return messages;
+    }
+}
+
+export { ListMessageClientUseCase }
