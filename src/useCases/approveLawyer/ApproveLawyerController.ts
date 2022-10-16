eb50849adc0cf66a18341afa34283f785e190030
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-10-16 16:29:10 -0300

    ref: ajustado API para apenas aprovar um cadastro de advogado.

--- src/useCases/approveLawyer/ApproveLawyerController.ts
+++ src/useCases/approveLawyer/ApproveLawyerController.ts
@@ -3,12 +3,11 @@ import { ApproveLawyerUseCase } from './ApproveLawyerUseCase';
 
 class ApproveLawyerController {
     async handle(request: Request, response: Response) {
-        const { id_usuario, id_advogado, aprovado } = request.body;
+        const { id_usuario, id_advogado } = request.body;
 
         const aproveLawyer = await new ApproveLawyerUseCase().execute({
             id_user: id_usuario,
-            id_lawyer: id_advogado,
-            aprove: aprovado,
+            id_lawyer: id_advogado
         });
 
         return response.status(200).json({ message: aproveLawyer });
