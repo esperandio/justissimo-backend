06290b66bfc931f8adb5b91e9ed47c21850ca135
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-12-04 20:20:50 -0300

    feat: implementacao de api para listar a configuracao de agenda do advogado

--- src/routes/lawyers/router.ts
+++ src/routes/lawyers/router.ts
@@ -4,6 +4,7 @@ import { ensureAuthenticated } from "../../middlewares/ensureAutheticated";
 import { ConfigScheduleController } from "../../useCases/config_schedule/ConfigScheduleController";
 import { CreateManualSchedulingController } from "../../useCases/createManualScheduling/CreateManualSchedulingController";
 import { CreateUserLawyerController } from "../../useCases/createUserLawyer/CreateUserLawyerController";
+import { ListConfigScheduleController } from "../../useCases/listConfigSchedule/ListConfigScheduleController";
 import { ListMessagesDivulgationLawyerController } from "../../useCases/listMessagesDivulgationLawyer/ListMessagesDivulgationLawyerController";
 import { ListAllLawyersController } from "../../useCases/listUserLawyer/listAllLawyers/ListAllLawyersController";
 import { ListLawyerByIdController } from "../../useCases/listUserLawyer/listLawyerById/ListLawyerByIdController";
@@ -13,11 +14,12 @@ import { ReviewLawyerController } from "../../useCases/reviewLawyer/ReviewLawyer
 const router = Router();
 
 /**Lawyers*/
-router.post('/lawyers', uploadImage.single('file'), new CreateUserLawyerController().handle);
 router.get("/lawyers", new ListAllLawyersController().handle);
 router.get("/lawyers/:id", new ListLawyerByIdController().handle);
 router.get("/lawyers/:fk_advogado/divulgation/:id", ensureAuthenticated, new ListMessagesDivulgationLawyerController().handle);
+router.get("/lawyers/:id/config-schedule", ensureAuthenticated, new ListConfigScheduleController().handle);
 
+router.post('/lawyers', uploadImage.single('file'), new CreateUserLawyerController().handle);
 router.post('/lawyers/:id/review', new ReviewLawyerController().handle);
 router.post('/lawyers/config-schedule', ensureAuthenticated, new ConfigScheduleController().handle);
 router.post("/lawyers/scheduling", ensureAuthenticated, new CreateManualSchedulingController().handle);
